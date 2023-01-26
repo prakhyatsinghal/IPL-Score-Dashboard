041dcdffbca45bed89ff9dae5e7f0de115343e0c
Prakhyat Singhal <singhalprakhyat2001@gmail.com>
2023-01-26 15:54:33 +0530

    Initialized Project

---
+++ src/main/java/com/iplscoredashboard/IplScoreDashBoardApplication.java
@@ -0,0 +1,13 @@
+package com.iplscoredashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplScoreDashBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IplScoreDashBoardApplication.class, args);
+    }
+
+}
